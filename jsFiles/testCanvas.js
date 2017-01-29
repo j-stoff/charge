a091e43565721d21b605a41b359fdce7aeb28a4b
j-stoff <jacob.ilikesports.stoffregen0@gmail.com>
2017-01-29 16:14:09 -0600

    Initial upload of original files

---
+++ jsFiles/testCanvas.js
@@ -0,0 +1,31 @@
+/*
+	Author: Jacob Stoffregen
+	Date: 1/22/17
+	Test canvas and background
+
+*/
+
+
+
+function runTest() {
+    "use strict";
+
+    /*
+    var canvas = document.getElementById("render-canvas");
+    var engine = new BABYLON.Engine(canvas);
+    var scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color3(0.8, 0.8, 0.8);
+    var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, -10), scene);
+    var light = new BABYLON.PointLight("light", new BABYLON.Vector3(10, 10, 0), scene);
+    var box = BABYLON.Mesh.CreateBox("box", 2, scene);
+    box.rotation.x = -0.2;
+    box.rotation.y = -0.4;
+    var boxMaterial = new BABYLON.StandardMaterial("material", scene);
+    boxMaterial.emissiveColor = new BABYLON.Color3(0, 0.58, 0.86);
+    box.material = boxMaterial;
+    var renderLoop = function () {
+        scene.render();
+    };
+    engine.runRenderLoop(renderLoop);
+    */  
+}
---
+++ jsFiles/testRun.js
@@ -0,0 +1,15 @@
+
+
+
+
+function testRun() {
+	
+	var id = document.getElementById("jsTest");
+	var output;
+
+
+	output = "It makes sense now";
+
+	id.innerHTML = output;
+
+}
